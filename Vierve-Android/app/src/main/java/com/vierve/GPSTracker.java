2129d5675b27c7bd7fc4cf538a2161a652c79c11
Kunal <kunal@KRP-Pro.local>
2017-08-24 21:54:43 -0700

    Added in correct files

---
+++ Vierve-Android/app/src/main/java/com/vierve/GPSTracker.java
@@ -0,0 +1,96 @@
+package com.vierve;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+
+
+public class GPSTracker extends Service implements LocationListener {
+
+    private final Context mContext;
+
+    Location location; // location
+    public static double latitude; // latitude
+    public static double longitude; // longitude
+
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 0; // 10 meters
+
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 3000;
+
+    // Declaring a Location Manager
+    protected LocationManager locationManager;
+
+    public GPSTracker(Context context) {
+        this.mContext = context;
+        getLocation();
+    }
+
+    public Location getLocation() {
+
+        locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(mContext, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        }
+        else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+            if (locationManager != null)
+            {
+                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (location != null)
+                {
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+//
+                }
+            }
+        }
+
+
+        return location;
+    }
+
+    @Override
+    public void onLocationChanged(Location location)
+    {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider)
+    {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider)
+    {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0)
+    {
+        return null;
+    }
+
+    public void endGPS() {
+        try {
+            locationManager.removeUpdates((LocationListener) mContext);
+            location = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
