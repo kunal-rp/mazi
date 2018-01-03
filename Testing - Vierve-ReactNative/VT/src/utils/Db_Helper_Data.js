facac87e18a2029f582152a6cd3c31b71a69d4a9
Brandon Palomino <12bpalomino@gmail.com>
2018-01-02 23:20:26 -0800

    finished db helpers

--- Testing - Vierve-ReactNative/VT/src/utils/Db_Helper_Data.js
+++ Testing - Vierve-ReactNative/VT/src/utils/Db_Helper_Data.js
@@ -1,8 +1,89 @@
 import {AsyncStorage} from 'react-native';
 
 class Db_Helper_Data{
-	static async clearAllData() {
-		
+	static async clearAllData(){
+		try{
+			await AsyncStorage.removeItem('college_info');
+			await AsyncStorage.removeItem('parkinglot_info');
+		} catch(error){
+			console.log(error);
+		}
+	}
+
+	static async addCollege(college){
+		let response = await AsyncStorage.getItem('college_info');
+		let collegeList = await JSON.parse(response) || []; // get list of colleges from storage
+		collegeList.push(college); // add college to list
+		await AsyncStorage.setItem('college_info', JSON.stringify(collegeList));
+	}
+
+	static async getAllCollegesInformation() {
+		let response = await AsyncStorage.getItem('college_info');
+		let collegeList = await JSON.parse(response) || [];
+		return collegeList;
+	}
+
+	static async checkCollege(id) {
+		let response = await AsyncStorage.getItem('college_info');
+		let collegeList = await JSON.parse(response) || [];
+		for (var i = 0; i < collegeList.length; i++) {
+			if(collegeList[i].college_id==id){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	static async getCollegeName(id) {
+		let response = await AsyncStorage.getItem('college_info');
+		let collegeList = await JSON.parse(response) || [];
+		for (i=0; i< collegeList.length; i++){
+			if(collegeList[i].college_id==id){
+				return collegeList[i].college_name;
+			}
+		}
+		return "";
+	}
+
+	static async getParkinglotName(id) {
+		let response = await AsyncStorage.getItem('parkinglot_info');
+		let parkingList = await JSON.parse(response) || [];
+		for (i=0; i< parkingList.length; i++){
+			if(parkingList[i].parkinglot_id==id){
+				return parkingList[i].parkinglot_name;
+			}
+		}
+		return "";
+	}
+
+	static async addParkingLot(parkingLot) {
+		let response = await AsyncStorage.getItem('parkinglot_info');
+		let parkingList = await JSON.parse(response) || [];
+		parkingList.push(parkingLot);
+		await AsyncStorage.setItem('parkinglot_info', JSON.stringify(parkingList));
+	}
+
+	static async getAllParkingLotsFromCollege(id) {
+		let response = await AsyncStorage.getItem('parkinglot_info');
+		let parkingList = await JSON.parse(response) || [];
+		var allParkingLots = [];
+		for (i = 0; i < parkingList.length; i++) {
+			if(parkingList[i].college_id==id){
+				allParkingLots.push(parkingList[i]);
+			}
+		}
+		return allParkingLots;
+	}
+
+	static async checkParkingLot(id) {
+		let response = await AsyncStorage.getItem('parkinglot_info');
+		let parkingList = await JSON.parse(response) || [];
+		for (var i = 0; i < parkingList.length; i++) {
+			if(parkingList[i].parkinglot_id==id){
+				return true;
+			}
+		}
+		return false;
 	}
 }
 
--- Testing - Vierve-ReactNative/VT/src/utils/Db_Helper_User.js
+++ Testing - Vierve-ReactNative/VT/src/utils/Db_Helper_User.js
@@ -3,36 +3,36 @@ import {AsyncStorage} from 'react-native';
 class Db_Helper_User{
 
 	static async getRemember() {
-		let response = await AsyncStorage.getItem('user_data');
-		let userData = await JSON.parse(response);
-		return userData.remember==1 ? true : false; // return true if remembered
+		let response = await AsyncStorage.getItem('user_info');
+		let userInfo = await JSON.parse(response);
+		return userInfo.remember==1 ? true : false; // return true if remembered
 	}
 
 	static async setRemember(value) {
 		// get data object
-		let response = await AsyncStorage.getItem('user_data');
-		let userData = await JSON.parse(response);
-		userData.remember=value; 	//set remember value
-		await AsyncStorage.setItem('user_data', JSON.stringify(userData)); //store back in storage
+		let response = await AsyncStorage.getItem('user_info');
+		let userInfo = await JSON.parse(response);
+		userInfo.remember=value; 	//set remember value
+		await AsyncStorage.setItem('user_info', JSON.stringify(userInfo)); //store back in storage
 	}
 
 	static async getInfo() {
-		let response = await AsyncStorage.getItem('user_data');
-		let userData = await JSON.parse(response);
-		return userData //return entire json object
+		let response = await AsyncStorage.getItem('user_info');
+		let userInfo = await JSON.parse(response);
+		return userInfo //return entire json object
 	}
 
 	static async clearAllData() {
 		try{
-			await AsyncStorage.removeItem('user_data');
+			await AsyncStorage.removeItem('user_info');
 		} catch(error){
 			console.log(error);
 		}
 	}
 
-	static async setUserInfo(userData) {
+	static async setUserInfo(userInfo) {
 		this.clearAllData();
-		await AsyncStorage.setItem('user_data', JSON.stringify(userData));
+		await AsyncStorage.setItem('user_info', JSON.stringify(userInfo));
 	}	
 }
 
