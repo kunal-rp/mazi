dcc1e2a3506a9f0748eb3e86c74f3464a52e02ad
Brandon Palomino <12bpalomino@gmail.com>
2018-01-08 23:18:46 -0800

    testing server tools functions in login and register screen

---
+++ Testing - Vierve-ReactNative/VT/src/utils/ServerTools.js
@@ -0,0 +1,53 @@
+class ServerTools{
+	// constructor() {
+	// 	this.url = "https://viervetesting.herokuapp.com/";
+	// }
+
+	async getCode() {
+		try{
+			let response = await fetch('https://viervetesting.herokuapp.com/codes');
+			let responseJson = await response.json();
+			// console.log(responseJson.general_key);
+			return responseJson.general_key;
+		} catch(error){
+			console.log(error);
+		}
+	}
+
+	async login(data) {
+		try{
+			let response = await fetch('https://viervetesting.herokuapp.com/login', {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(data),
+			});
+			let responseJson = await response.json();
+			return responseJson;
+		} catch(error){
+			console.log(error);
+		}
+	}
+
+	async createUser(data) {
+		try{
+			let response = await fetch('https://viervetesting.herokuapp.com/createUser', {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(data),
+			});
+			let responseJson = await response.json();
+			console.log(responseJson);
+			return responseJson;
+		} catch(error){
+			console.log(error);
+		}
+	}
+}
+
+export default new ServerTools;
